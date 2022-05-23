51924bde422a091aee22b5a78a1ce389b9a91b65
irfanalibot0 <104135320+irfanalibot0@users.noreply.github.com>
2022-05-23 21:38:57 +0530

    Add files via upload

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,17 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.ListCommand = 'find'
+        self.CloneCommand = 'clone'
+        self.CountCommand = 'count'
+        self.PermissionCommand = 'perm'
+        self.DeleteCommand = 'del'
+        self.AuthorizeCommand = 'authorize'
+        self.UnauthorizeCommand = 'unauthorize'
+        self.UsersCommand = 'users'
+        self.ShellCommand = 'shell'
+        self.PingCommand = 'ping'
+        self.LogCommand = 'log'
+        self.HelpCommand = 'help'
+
+BotCommands = _BotCommands()
